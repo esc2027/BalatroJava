e94e1251d43115f55759a3f22d5fd73e2e243c88
esc2027 <esoule-cebulski_2027@student-bmhs-la.org>
2026-04-02 12:54:02 -0700

    Edit banner/text color palette for better display in terminal

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -2,13 +2,13 @@ public class Color {
     public static final String RESETCOLOR = "\u001B[0m";
 
     public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
+    public static final String RED = "\u001b[38;2;255;66;63m";
     public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
+    public static final String YELLOW = "\u001b[38;2;242;203;0m";
+    public static final String BLUE = "\u001b[38;2;5;130;220m";
     public static final String PURPLE = "\u001B[35m";
     public static final String WHITE = "\u001B[97m";
-    public static final String ORANGE = "\u001b[38;2;220;130;0m";
+    public static final String ORANGE = "\u001b[38;2;190;120;0m";
 
     public static final String REDBG = "\u001B[41m";
     public static final String YELLOWBG = "\u001B[43m";
